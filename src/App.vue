8a9a2ced2ff4f378b16f30a1a7fe1224d9f9cd62
Christian <christian@monostream.com>
2023-08-26 13:53:49 +0200

    add shadow

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from "vue";
 import { IfcViewerAPI } from "web-ifc-viewer";
 import { BoxGeometry, Color, Mesh, MeshPhongMaterial, PCFSoftShadowMap, Vector2, Vector3 } from "three";
 import { getUV } from "./services/weather";
+import { SunLight } from "./sun";
 
 const container = ref<HTMLDivElement | null>(null);
 
@@ -17,8 +18,16 @@ const createBox = (width: number, height: number, depth: number, color = 0xfffff
 	return cube;
 }
 
+const sun = new SunLight(
+  // Bern
+  new Vector2( 46.9480, 7.4474 ),
+  // TODO: Find north
+  new Vector3( 0.0, 0.0, 1.0 ),
+  new Vector3( -1.0, 0.0, 0.0 ),
+  new Vector3( 0.0, -1.0, 0.0 )
+);
+const date = new Date();
 const uv = ref(0)
-
 
 onMounted(async () => {
   if (!container.value) {
@@ -38,8 +47,25 @@ onMounted(async () => {
     COORDINATE_TO_ORIGIN: true
   });
 
-  viewer.axes.setAxes();
-  viewer.grid.setGrid();
+  // viewer.axes.setAxes();
+  // viewer.grid.setGrid();
+
+  viewer.context.renderer.renderer.shadowMap.enabled = true;
+
+  // const ground = createBox( 50.0, 1.0, 50.0, 0x99ff99 );
+  // ground.position.set( 0.0, 0.0, 0.0 );
+  // viewer.context.scene.add( ground );
+  // // Cube in the middle
+  // const cube = createBox( 5.0, 5.0, 5.0 );
+  // cube.position.set( 0.0, 5.0, 0.0 );
+  // viewer.context.scene.add( cube );
+  // // North pole indicator
+  // const northIndicator = createBox( 2.0, 2.0, 8.0, 0xff0000 );
+  // northIndicator.position.set( 0.0, 1.0, 20.0 );
+  // viewer.context.scene.add( northIndicator );
+
+  // Add an ambient light
+  // viewer.context.scene.add(new AmbientLight( 0x333333));
 
   window.ondblclick = async () => {
     const item = await viewer.IFC.selector.pickIfcItem(true)
@@ -58,50 +84,66 @@ onMounted(async () => {
     const y = coords[1].value
     const z = coords[2].value
 
+    if (!x || !y || !z) {
+      console.log('coordinates not available')
+      return
+    }
+
     console.log({x, y, z})
   };
-  window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+  // window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
   viewer.clipper.active = true;
 
+  viewer.context.renderer.renderer.shadowMap.enabled = true;
+  viewer.context.renderer.renderer.shadowMap.type = PCFSoftShadowMap; // default THREE.PCFShadowMap
 
-  // const sun = new DirectionalLight(0xffffff, 1); // white color, intensity 1
-  // sun.position.set(100, 100, 100); // set the light's position
-  // sun.castShadow = true; // enable shadow casting
-  // viewer.context.scene.add(sun); // add the light to the scene
-
-
-  // sun.shadow.mapSize.width = 512; // default is 512
-  // sun.shadow.mapSize.height = 512; // default is 512
+  // sun.shadow.mapSize.width = 51200; // default is 512
+  // sun.shadow.mapSize.height = 51200; // default is 512
   // sun.shadow.camera.near = 0.5; // default is 0.5
-  // sun.shadow.camera.far = 500; // default is 500
-
-
-  // viewer.context.renderer.renderer.shadowMap.enabled = true;
-  // viewer.context.renderer.renderer.shadowMap.type = PCFSoftShadowMap; // default THREE.PCFShadowMap
+  // sun.shadow.camera.far = 50000; // default is 500
 })
 
-const changed = (changed: Event) => {
-    if (!changed.target) {
-      return
-    }
-
-    const input = changed.target as HTMLInputElement;
-
-    if (!input.files || !input.files.length) {
-      return
-    }
-
-    const file = input.files[0];
-
-    const ifcURL = URL.createObjectURL(file);
-
-    viewer.IFC.loadIfcUrl(ifcURL);
+const changed = async (changed: Event) => {
+  if (!changed.target) {
+    return
   }
+
+  const input = changed.target as HTMLInputElement;
+
+  if (!input.files || !input.files.length) {
+    return
+  }
+
+  const file = input.files[0];
+
+  const ifcURL = URL.createObjectURL(file);
+
+  await viewer.IFC.loadIfcUrl(ifcURL);
+
+  viewer.context.scene.scene.castShadow = true
+  viewer.context.scene.scene.children.forEach(c => {
+    if (c.type === "Mesh") {
+      c.castShadow = true
+      c.receiveShadow = true
+    }
+  })
+
+  viewer.context.scene.add(sun)
+
+	sun.directionalLight.shadow.camera.right = 300.0;
+	sun.directionalLight.shadow.camera.left = -30.0;
+	sun.directionalLight.shadow.camera.top = 30.0;
+	sun.directionalLight.shadow.camera.bottom = -30.0;
+
+	sun.updateOrientation(date);
+	sun.updateDirectionalLight();
+}
+
 const goTo = () => {
   // Cube in the middle
-  const cube = createBox( 5.0, 5.0, 5.0, 0xff0000 );
-  cube.position.set( 0.0, 5.0, 0.0 );
-  viewer.context.scene.add( cube );
+  const cube = createBox(5.0, 5.0, 5.0, 0xff0000);
+  cube.position.set(0.0, 5.0, 0.0);
+  viewer.context.scene.add(cube);
 
   viewer.context.ifcCamera.cameraControls.setLookAt(50, 50, 50, 0, 5, 0, true)
 }
